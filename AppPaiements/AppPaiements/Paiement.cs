3de70cc302b61a8571ab8d3ee4c21e8ba4d20c58
Modeste Ewinsou <300135987@monboreal.ca>
2024-10-19 21:00:58 -0400

    ajout de la classe Paiement

---
+++ AppPaiements/AppPaiements/Paiement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paiement
+    {
+        // Propriétés 
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+
+        // Constructeur
+        public Paiement(double montant, string description)
+        {
+            Montant = montant;
+            Description = description;
+
+        }
+
+        // Méthode virtuelle 
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Montant :{Montant},Description:{Description}");
+
+        }
+
+    }
+}
