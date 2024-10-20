90fddcfc4773b9be461a2f4989244d02faed55b4
Modeste Ewinsou <300135987@monboreal.ca>
2024-10-19 21:47:18 -0400

    ajout de la classe CarteCredit et modification de Programs main

--- AppPaiements/AppPaiements/Program.cs
+++ AppPaiements/AppPaiements/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, Mr Younes");
+        CarteCredit paiement1 = new CarteCredit(124.00, "Achat a sportchek", 76346367);
+        CarteCredit paiement2 = new CarteCredit(381.00, "Achat assurance voiture", 2377661);
+        CarteCredit paiement3 = new CarteCredit(25.00, "Achat Pizza", 8887);
+
+        paiement1.AfficherDetails();
+        paiement2.AfficherDetails();
+        paiement3.AfficherDetails();
+
     }
 }
